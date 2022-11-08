2059b615dbe95b25af3534fa8160c8f9d7b8a108
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-08 12:08:22 -0600

    create game class with game properties

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor(player, ) {
+    this.player1 = new Player (1, '☀️');
+    this.player2 = new Player (2, '🌧');
+    this.board1 = [];
+    this.board2 = [];
+    this.winner =  ``;
+    this.draw = false;
+  }
+}
