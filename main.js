bab49883af30ad5177b70c1559777e767906b82c
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-08 16:12:12 -0600

    start adding query selectors for planning

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+var player1Score = document.querySelector('.player1-score');
+var player2Score = document.querySelector('.player2-score');
+var clearGameButton = document.querySelector('.clear-game-button');
+var gameBoard = document.querySelector('.game-board');
+var gameHeader = document.querySelector('h1');
+
+
+
+
+// playe game - target box by classlist or id
