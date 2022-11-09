63e225878680ae752b9a28f9344f254845fb86d1
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-09 13:28:53 -0600

    update playRound function

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
+var game = new Game()
 
-
-
+var box1 = document.querySelector('#box-1');
+var box2 = document.querySelector('#box-2');
+var box3 = document.querySelector('#box-3');
+var box4 = document.querySelector('#box-4');
+var box5 = document.querySelector('#box-5');
+var box6 = document.querySelector('#box-6');
+var box7 = document.querySelector('#box-7');
+var box8 = document.querySelector('#box-8');
+var box9 = document.querySelector('#box-9');
 var player1Score = document.querySelector('.player1-score');
 var player2Score = document.querySelector('.player2-score');
 var clearGameButton = document.querySelector('.clear-game-button');
 var gameBoard = document.querySelector('.game-board');
 var gameHeader = document.querySelector('h1');
 
-
 var game;
 var currentPlayer;
-
-var game = new Game()
-console.log(game)
 
 function determineTurn() {
   if (game.player1.choices.length <= game.player2.choices.length) {
@@ -20,11 +24,11 @@ function determineTurn() {
   } else {
     currentPlayer = game.player2
   }
-}
+};
 
-function playRound(choice) {
+function playRound(turn) {
   determineTurn()
-  game.playGame(choice, currentPlayer)
+  game.playGame(turn, currentPlayer)
     if (!game.gameOver && !game.draw) {
       determineTurn()
     } if (game.gameOver) {
@@ -34,6 +38,5 @@ function playRound(choice) {
       console.log(`It's a draw!`)
       game.resetGame()
     }
-} 
-
+};
 
