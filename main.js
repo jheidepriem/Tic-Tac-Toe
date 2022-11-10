60e2586bdbdb4e640a13d0c2ebbd0f9d12544d66
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-10 17:24:02 -0600

    refactor playRound function and addPlayersToken. Both functions are now working, connecting the data-model and the DOM. Innertext is changing for players turn, as well their score, and game piece.

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
 var game = new Game()
 
-var box = document.getElementById('#box')
+var boxes = document.querySelectorAll('.box');
 var player1Score = document.querySelector('.player1-score');
 var player2Score = document.querySelector('.player2-score');
 var clearGameButton = document.querySelector('.clear-game-button');
 var gameBoard = document.querySelector('.game-board');
 var gameHeader = document.querySelector('h1');
 
-window.addEventListener('load', loadGame)
-
-function loadGame() {
-
-}
+gameBoard.addEventListener('click', addPlayersToken)
+gameHeader.innerText = `It's Player 1's Turn!`
 
 function playRound(choice) {
   game.playGame(choice) 
-  console.log(game.player1.choices)
-  console.log(game.player2.choices)
+  updateMessageAndWins()
+  showButton() 
 }
 
-function pickGamePiece(event) {
-  game.currentPlayer.choice = event.target.id;
-  box.innerText = `game.currentPlayer.token`  
-}
+function addPlayersToken(event) {
+  var choice = Number(event.target.id)
+  playRound(choice);
+  if (game.currentPlayer.choices.includes(choice)) {
+      boxes[choice].innerText = `${game.currentPlayer.token}`;
+      boxes[choice].classList.add('disabled')
+  }
+
+};
 
 function updateMessageAndWins() {
+  var nextPlayer = game.currentPlayer.id === game.player1.id ? game.player2.id : game.player1.id
+  gameHeader.innerText = `It's Player ${nextPlayer}'s Turn!`
   if (game.player1.wins) {
    gameHeader.innerText = `Player ${game.player1.id} Wins!`
   } else if (game.player2.wins) {
     gameHeader.innerText = `Player ${game.player2.id} Wins!`
-  } else {
-    gameHeader.innerText = `It's a Draw!`
   }
   player1Score.innerText = `Wins: ${game.player1.wins}`
   player2Score.innerText = `Wins: ${game.player2.wins}`
-}
-
-function displayPlayersTurn() {
-  gameHeader.innerText = `Player ${game.currentPlayer.id}'s Turn!`
-}
+};  
 
 function displayReset() {
-  game.resetGame()
-  setTimeout(game.resetGame, 1800);
-  player1Score.innerText = ''
-  player2Score.innerText = ''
+  if(game.gameOver = true) {
+    setTimeout(clearBoard, 1500);
+  }
 }
+ 
+function showButton() {
+  clearGameButton.classList.remove('hidden')
+};
 
+function clearBoard() {
+  game.resetGameData() 
+  boxes.innerHTML = ``
+};
+
+// function clearScores() {
+//   game.resetGameData()
+//   player1Score.innerText = `Wins: `
+//   player2Score.innerText = `Wins: `
+//   clearGameButton.classList.add('hidden')
+// };
