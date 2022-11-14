3ecc39b61308eff7e5ea56f2963a6dc14166ab7b
Josephine Heidepriem <jheidepriem@gmail.com>
2022-11-14 16:16:29 -0600

    add small change to determine winner function in order to add user wins to local storage

--- player.js
+++ player.js
@@ -5,12 +5,35 @@ class Player {
     this.wins = 0;
     this.choice = null;
     this.choices = [];
-  }
+};
 
 takeTurn(choice) {
   this.choice = choice
+};
+
+saveWinsToStorage() {
+  if (this.id === 1) {
+    localStorage.setItem("p1SavedWins", this.wins);
+    } else if (this.id === 2) {
+    localStorage.setItem("p2SavedWins", this.wins);
+    }
+  };
+
+  retrieveWinsFromStorage() {
+  if (this.id === 1) {
+    this.wins = localStorage.getItem('p1SavedWins')
+  } else {
+    this.wins = localStorage.getItem('p2SavedWins')
+    }
   }
 };
+
+
+
+
+
+
+
 
 
 
